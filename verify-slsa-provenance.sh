1ab562a7c68c6d1564cf95377fe5f1d563923d82
Drew Roen <102626803+drewroengoogle@users.noreply.github.com>
2023-01-23 13:42:07 -0600

    Update verify-slsa-provenance.sh

--- verify-slsa-provenance.sh
+++ verify-slsa-provenance.sh
@@ -10,7 +10,7 @@ go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier@v2.0.1
 
 echo "Verifying the provenance is valid and correct..."
 slsa-verifier verify-image $(cat DOCKER_DIGEST_URL) \
-  --source-uri https://github.com/drewroengoogle/test-script2 \
+  --source-uri https://github.com/drewroengoogle/test-script \
   --builder-id=https://cloudbuild.googleapis.com/GoogleHostedWorker@v0.3 \
   --provenance-path unverified-provenance.json
   
