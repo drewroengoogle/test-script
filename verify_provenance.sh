1f264febd0340387d75878c4dac944448104cfe0
Drew Roen <drewroen@google.com>
2023-03-01 10:54:53 -0600

    Update test to retry

--- verify_provenance.sh
+++ verify_provenance.sh
@@ -3,16 +3,19 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This script is used to verify provenance of our artifacts using slsa-verifier.
+# If slsa-verifier is unable to ensure the provenance of the artifact is
+# legitimate, then the script will exit with a non-zero exit code.
 set -e
 
 # Download the jq binary in order to obtain the artifact registry url from the
 # docker image provenance.
-echo "Installing jq using curl..."
+echo "Installing jq using apt..."
 apt update && apt install jq=1.5+dfsg-1.3 -y
 
 # Download slsa-verifier in order to validate the docker image provenance.
 echo "Installing slsa-verifier using go..."
-go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier@a43888265e1f6aae98c924538298944f2721dcf0
+go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier@a43888265e1f6aae98c924538298944f2721dcf0 #v2.0.1: https://github.com/slsa-framework/slsa-verifier/releases/tag/v2.0.1
 
 # This command uses slsa-verifier to ensure the provenance has the correct
 # source location and builder.
@@ -23,12 +26,9 @@ go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier@a4388826
 # Note: jq is used in order to obtain the full artifact registry url from
 # the provenance metadata.
 echo "Verifying the provenance is valid and correct..."
-ls  
-FULLY_QUALIFIED_DIGEST=$(cat unverified-provenance.json | \
+FULLY_QUALIFIED_DIGEST=$(cat $1 | \
   jq -r .image_summary.fully_qualified_digest)
 slsa-verifier verify-image $FULLY_QUALIFIED_DIGEST \
   --source-uri https://github.com/drewroengoogle/test-script \
   --builder-id=https://cloudbuild.googleapis.com/GoogleHostedWorker@v0.3 \
-  --provenance-path unverified-provenance.json
-
-echo "Provenance has been successfully validated."
+  --provenance-path $1
