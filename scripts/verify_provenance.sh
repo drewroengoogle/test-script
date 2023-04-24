309cd64e6f7b1c85ed30b5c9ee90a554845b6851
Drew Roen <102626803+drewroengoogle@users.noreply.github.com>
2023-04-24 12:59:38 -0500

    Update verify_provenance.sh

--- scripts/verify_provenance.sh
+++ scripts/verify_provenance.sh
@@ -13,51 +13,53 @@ SOURCE_URI=https://github.com/drewroengoogle/test-script2
 # Download the jq binary in order to obtain the artifact registry url from the
 # docker image provenance.
 echo "Installing jq using apt..."
-sudo apt-get install jq
+apt-get install jq
 
-# Download slsa-verifier in order to validate the docker image provenance.
-# This takes the version of slsa-verifier defined in tooling/go.mod.
-echo "Installing slsa-verifier using go..."
-pushd tooling
-go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier
-popd
+# # Download slsa-verifier in order to validate the docker image provenance.
+# # This takes the version of slsa-verifier defined in tooling/go.mod.
+# echo "Installing slsa-verifier using go..."
+# pushd tooling
+# go install github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier
+# popd
 
-FULLY_QUALIFIED_DIGEST=$(cat $PROVENANCE_PATH |
-  jq -r .image_summary.fully_qualified_digest)
+# FULLY_QUALIFIED_DIGEST=$(cat $PROVENANCE_PATH |
+#   jq -r .image_summary.fully_qualified_digest)
 
-# This command uses slsa-verifier to ensure the provenance has the correct
-# source location and builder.
-# "source-uri" is the original location of the source code
-# "builder-id" is where the artifact was built (Note: GoogleHostedWorker is
-# a GCP Cloud Build instance)
-#
-# Note: jq is used in order to obtain the full artifact registry url from
-# the provenance metadata.
-echo "Verifying the provenance is valid and correct..."
-echo "Checking for source-uri of https://github.com/flutter/cocoon"
-slsa-verifier verify-image $FULLY_QUALIFIED_DIGEST \
-  --source-uri git+$SOURCE_URI \
-  --builder-id=$BUILDER_ID \
-  --provenance-path $PROVENANCE_PATH
+# # This command uses slsa-verifier to ensure the provenance has the correct
+# # source location and builder.
+# # "source-uri" is the original location of the source code
+# # "builder-id" is where the artifact was built (Note: GoogleHostedWorker is
+# # a GCP Cloud Build instance)
+# #
+# # Note: jq is used in order to obtain the full artifact registry url from
+# # the provenance metadata.
+# echo "Verifying the provenance is valid and correct..."
+# echo "Checking for source-uri of https://github.com/flutter/cocoon"
+# slsa-verifier verify-image $FULLY_QUALIFIED_DIGEST \
+#   --source-uri git+$SOURCE_URI \
+#   --builder-id=$BUILDER_ID \
+#   --provenance-path $PROVENANCE_PATH
 
-# If the provenance failed, try again, but check for 'git+' in the source-uri
-# Context: Cloud Build is sometimes generating provenance with 'git+', but it
-# will eventually be generated for all builds.
-COMMAND_RESULT=$?
-if [[ $COMMAND_RESULT -eq 0 ]]; then
-  echo "Provenance verified!" && exit $COMMAND_RESULT
-fi
+# # If the provenance failed, try again, but check for 'git+' in the source-uri
+# # Context: Cloud Build is sometimes generating provenance with 'git+', but it
+# # will eventually be generated for all builds.
+# COMMAND_RESULT=$?
+# if [[ $COMMAND_RESULT -eq 0 ]]; then
+#   echo "Provenance verified!" && exit $COMMAND_RESULT
+# fi
 
-echo "Verifying the provenance is valid and correct..."
-echo "Checking for source-uri of git+https://github.com/flutter/cocoon"
-slsa-verifier verify-image $FULLY_QUALIFIED_DIGEST \
-  --source-uri $SOURCE_URI \
-  --builder-id=$BUILDER_ID \
-  --provenance-path $PROVENANCE_PATH
+# echo "Verifying the provenance is valid and correct..."
+# echo "Checking for source-uri of git+https://github.com/flutter/cocoon"
+# slsa-verifier verify-image $FULLY_QUALIFIED_DIGEST \
+#   --source-uri $SOURCE_URI \
+#   --builder-id=$BUILDER_ID \
+#   --provenance-path $PROVENANCE_PATH
 
-COMMAND_RESULT=$?
-if [[ $COMMAND_RESULT -eq 0 ]]; then
-  echo "Provenance verified!" && exit $COMMAND_RESULT
-fi
+# COMMAND_RESULT=$?
+# if [[ $COMMAND_RESULT -eq 0 ]]; then
+#   echo "Provenance verified!" && exit $COMMAND_RESULT
+# fi
 
-echo "Failed to validate provenance." && exit $COMMAND_RESULT
+# echo "Failed to validate provenance." && exit $COMMAND_RESULT
+
+exit $COMMAND_RESULT
