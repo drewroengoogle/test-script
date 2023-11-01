26196de2f48b2f570513dcdd67c92e253a3bca6a
Drew Roen <drewroen@google.com>
2023-11-01 12:39:02 -0500

    move to shell

---
+++ cloud_build/scripts/stage_site_and_comment_on_github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Deploying website to a staging channel on firebase..."
+FIREBASE_DEPLOY_RESPONSE=$(firebase hosting:channel:deploy --expires 7d pr$_PR_NUMBER-$_HEAD_BRANCH --project=$PROJECT_ID)
+FIREBASE_STAGING_URL=$(echo $$FIREBASE_DEPLOY_RESPONSE | grep -Eo "https://$PROJECT_ID--[a-zA-Z0-9./?=_%:-]*")
+
+echo "Logging into github under bot account..."
+echo $$GH_PAT_TOKEN > token
+gh auth login --with-token < token
+
+echo "Commenting staging url on PR..."
+PR_BODY=\
+"Visit the preview URL for this PR (updated for commit $COMMIT_SHA):
+
+$$FIREBASE_STAGING_URL"
+
+gh pr comment $_PR_NUMBER --edit-last --body "$$PR_BODY" --repo $REPO_FULL_NAME || \
+    gh pr comment $_PR_NUMBER --body "$$PR_BODY" --repo $REPO_FULL_NAME
