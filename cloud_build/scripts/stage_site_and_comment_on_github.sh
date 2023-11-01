64b8ab7a1d3b73292e05471d52f1c7c1392b350a
Drew Roen <drewroen@google.com>
2023-11-01 12:47:18 -0500

    Pass in a different way

--- cloud_build/scripts/stage_site_and_comment_on_github.sh
+++ cloud_build/scripts/stage_site_and_comment_on_github.sh
@@ -4,11 +4,14 @@ set -e
 ls
 
 echo "Deploying website to a staging channel on firebase..."
-echo $_PR_NUMBER
-echo $_HEAD_BRANCH
-echo $PROJECT_ID
-firebase hosting:channel:deploy --expires 7d pr$_PR_NUMBER-$_HEAD_BRANCH --project=$PROJECT_ID
-FIREBASE_DEPLOY_RESPONSE=$(firebase hosting:channel:deploy --expires 7d pr$_PR_NUMBER-$_HEAD_BRANCH --project=$PROJECT_ID)
+PR_NUMBER=$1
+HEAD_BRANCH=$2
+PROJECT_ID=$3
+COMMIT_SHA=$4
+REPO_FULL_NAME=$5
+
+firebase hosting:channel:deploy --expires 7d pr$PR_NUMBER-$HEAD_BRANCH --project=$PROJECT_ID
+FIREBASE_DEPLOY_RESPONSE=$(firebase hosting:channel:deploy --expires 7d pr$PR_NUMBER-$HEAD_BRANCH --project=$PROJECT_ID)
 FIREBASE_STAGING_URL=$(echo $$FIREBASE_DEPLOY_RESPONSE | grep -Eo "https://$PROJECT_ID--[a-zA-Z0-9./?=_%:-]*")
 
 echo "Logging into github under bot account..."
@@ -21,5 +24,5 @@ PR_BODY=\
 
 $$FIREBASE_STAGING_URL"
 
-gh pr comment $_PR_NUMBER --edit-last --body "$$PR_BODY" --repo $REPO_FULL_NAME || \
-    gh pr comment $_PR_NUMBER --body "$$PR_BODY" --repo $REPO_FULL_NAME
+gh pr comment $PR_NUMBER --edit-last --body "$$PR_BODY" --repo $REPO_FULL_NAME || \
+    gh pr comment $PR_NUMBER --body "$$PR_BODY" --repo $REPO_FULL_NAME
