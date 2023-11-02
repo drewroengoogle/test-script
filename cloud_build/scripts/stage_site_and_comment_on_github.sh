fc2c2232bbad6ee81e06df608f8e6ed8d2f13b62
Drew Roen <drewroen@google.com>
2023-11-02 11:49:33 -0500

    Pass variables directly

--- cloud_build/scripts/stage_site_and_comment_on_github.sh
+++ cloud_build/scripts/stage_site_and_comment_on_github.sh
@@ -4,7 +4,7 @@ set -e
 deploy_to_firebase_staging_channel () {
     echo "Deploying website to a staging channel on firebase..."
 
-    FIREBASE_DEPLOY_RESPONSE=$(firebase hosting:channel:deploy --expires 7d pr$_PR_NUMBER-$HEAD_BRANCH --project=$PROJECT_ID)
+    FIREBASE_DEPLOY_RESPONSE=$(firebase hosting:channel:deploy --expires 7d pr$PR_NUMBER-$HEAD_BRANCH --project=$PROJECT_ID)
     echo "$FIREBASE_DEPLOY_RESPONSE"
     FIREBASE_STAGING_URL=$(grep -Eo "https://$PROJECT_ID--[a-zA-Z0-9./?=_%:-]*" <<< "$FIREBASE_DEPLOY_RESPONSE")
 }
@@ -23,8 +23,8 @@ comment_staging_url_on_github () {
     # The github CLI throws an error if --edit-last doesn't find a previous
     # comment, so this edits the last comment, but if it doesn't exist,
     # leave a new comment.
-    gh pr comment $_PR_NUMBER --edit-last --body-file PR_BODY --repo $REPO_FULL_NAME || \
-        gh pr comment $_PR_NUMBER ---body-file PR_BODY --repo $REPO_FULL_NAME
+    gh pr comment $PR_NUMBER --edit-last --body-file PR_BODY --repo $REPO_FULL_NAME || \
+        gh pr comment $PR_NUMBER ---body-file PR_BODY --repo $REPO_FULL_NAME
 }
 
 PR_NUMBER=$1
@@ -32,6 +32,7 @@ HEAD_BRANCH=$2
 PROJECT_ID=$3
 COMMIT_SHA=$4
 REPO_FULL_NAME=$5
+GH_PAT_TOKEN=$6
 
 deploy_to_firebase_staging_channel
 login_to_github
